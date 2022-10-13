6c94da31ff1f0755959ddbf393b88429dc6d9f5f
juzhteen <juzhteenrivero@gmail.com>
2022-10-13 15:03:31 +0800

    Add homepage and request document page

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/request/docs', function () {
+    return view('docsrequest');
+});
+
 // Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 //     return view('dashboard');
 // })->name('dashboard');
