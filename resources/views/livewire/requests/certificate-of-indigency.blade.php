ccc45be5c52e927305b218140a0d7a42691a8df5
juzhteen <juzhteenrivero@gmail.com>
2022-11-18 15:47:30 +0800

    Editable issued date on indigency print

--- resources/views/livewire/requests/certificate-of-indigency.blade.php
+++ resources/views/livewire/requests/certificate-of-indigency.blade.php
@@ -9,15 +9,45 @@
     certify further that the above-named person belongs to an indigent family of this Barangay.</p>
 <p class="mt-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This
     certification is being issued for whatever legal purposes that may serve him/her best.</p>
-<p class="mt-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Issued this
-    {{ now()->day }} of {{ now()->format('F') }}, {{ now()->year }} at the Office of the Barangay Chairman,
-    Barangay Paulino Labio, Northern Kabuntalan, Maguindanao.</p>
+<p x-data="data()" class="mt-4">
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Issued this
+    <a @click.prevent @dblclick="toggleEditingState" x-show="!isEditing" x-text="text"
+        class="select-none cursor-pointer underline text-black"></a>
+    <input type="text" x-model="text" x-show="isEditing" @click.away="toggleEditingState"
+        @keydown.enter="disableEditing" @keydown.window.escape="disableEditing"
+        class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-l px-4 appearance-none leading-normal w-128"
+        x-ref="input"> at the Office of the Barangay Chairman,
+    Barangay Paulino Labio, Northern Kabuntalan, Maguindanao.
+</p>
 <p class="text-right mt-40 underline">
     @php
         $bc = App\Models\Employee::join('employee_types', 'employees.employee_type_employee_type_id', '=', 'employee_types.employee_type_id')
             ->where('employee_types.position', 'Barangay Chairman')
             ->first();
-        echo $bc->resident->name;
+        if($bc){
+            echo $bc->resident->name;
+        }
     @endphp
 </p>
 <p class="text-right">Punong Barangay</p>
+
+<script>
+    function data() {
+        return {
+            text: "Double click to enter date",
+            isEditing: false,
+            toggleEditingState() {
+                this.isEditing = !this.isEditing;
+
+                if (this.isEditing) {
+                    this.$nextTick(() => {
+                        this.$refs.input.focus();
+                    });
+                }
+            },
+            disableEditing() {
+                this.isEditing = false;
+            }
+        };
+    }
+</script>
